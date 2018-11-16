01ffe425e54f676e03053dbe9d23c35b1908dc61
An Cao <kcj.pop.com@gmail.com>
2018-11-16 12:22:56 +0200

    Fix publicPath when building
    Signed-off-by: An Cao <kcj.pop.com@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,22 @@
-const path = require('path');
-const outputDir = path.join(__dirname, 'build/');
+const path = require('path')
+const outputDir = path.join(__dirname, 'build/')
 
-const PnpWebpackPlugin = require('pnp-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const PnpWebpackPlugin = require('pnp-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-const isProd = process.env.NODE_ENV === 'production';
+const isProd = process.env.NODE_ENV === 'production'
 
 const devServer = {
   contentBase: outputDir,
   historyApiFallback: true
-};
+}
 
 module.exports = {
   entry: './src/Index.bs.js',
   mode: isProd ? 'production' : 'development',
   output: {
     path: outputDir,
-    publicPath: outputDir,
+    publicPath: '/',
     filename: 'Index.js'
   },
   resolve: {
@@ -31,4 +31,4 @@ module.exports = {
       template: path.resolve(__dirname, 'src/index.html')
     })
   ]
-};
+}
